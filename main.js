0c6e124fdd6360f9226b917cb73428c2979a4ecc
Zaidsyed123 <71397217+Zaidsyed123@users.noreply.github.com>
2022-10-17 09:04:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -13,42 +13,23 @@ function btnClicked() {
   let d3Limit = +document.getElementById('d3-limit').value;
 
   // Calculate & Output Ticket Types
-  // Driver 1
-  let d1Ticket;
-  if (d1Speed > d1Limit + 40) {
-    d1Ticket = 'Really Big Ticket';
-  } else if (d1Speed > d1Limit + 20) {
-    d1Ticket = 'Big Ticket';
-  } else if (d1Speed > d1Limit) {
-    d1Ticket = 'Small Ticket';
-  } else {
-    d1Ticket = 'No Ticket';
-  }
-  document.getElementById('d1-ticket').innerHTML = d1Ticket;
-
-  // Driver 2
-  let d2Ticket;
-  if (d2Speed > d2Limit + 40) {
-    d2Ticket = 'Really Big Ticket';
-  } else if (d2Speed > d2Limit + 20) {
-    d2Ticket = 'Big Ticket';
-  } else if (d2Speed > d2Limit) {
-    d2Ticket = 'Small Ticket';
-  } else {
-    d2Ticket = 'No Ticket';
-  }
-  document.getElementById('d2-ticket').innerHTML = d2Ticket;
-
-  // Driver 3
-  let d3Ticket;
-  if (d3Speed > d3Limit + 40) {
-    d3Ticket = 'Really Big Ticket';
-  } else if (d3Speed > d3Limit + 20) {
-    d3Ticket = 'Big Ticket';
-  } else if (d3Speed > d3Limit) {
-    d3Ticket = 'Small Ticket';
-  } else {
-    d3Ticket = 'No Ticket';
-  }
-  document.getElementById('d3-ticket').innerHTML = d3Ticket;
+  document.getElementById('d1-speed').innerHTML = speedToFine(d1Speed);
+  document.getElementById('d1-limit').innerHTML = speedToFine(d1Limit);
+  document.getElementById('d2-speed').innerHTML = speedToFine(d2Speed);
+  document.getElementById('d2-limit').innerHTML = speedToFine(d2Limit);
+  document.getElementById('d3-speed').innerHTML = speedToFine(d3Speed);
+  document.getElementById('d3-limit').innerHTML = speedToFine(d3Limit);
 }
+function speedToFine(speedLimit) {
+  if (speedLimit > speedLimit + 40) {
+    return 'Really Big Ticket';
+  } else if (speedLimit > speedLimit + 20) {
+    return 'Big Ticket';
+  } else if (speedLimit > speedLimit) {
+    return 'Small Ticket';
+  } else {
+    return 'No Ticket';
+  }
+}
+
+

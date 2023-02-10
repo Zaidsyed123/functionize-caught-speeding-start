8b0fb9fe63d9616cef61e0829bc69fdfb73143c1
Zaidsyed123 <71397217+Zaidsyed123@users.noreply.github.com>
2023-02-10 10:43:42 -0700

    i

--- main.js
+++ main.js
@@ -4,28 +4,36 @@
 document.getElementById('btn').addEventListener('click', btnClicked);
 
 function btnClicked() {
+  
   // Get Speed & Speed Limit Inputs
-  let d1Speed = +document.getElementById('d1-speed').value;
-  let d1Limit = +document.getElementById('d1-limit').value;
-  let d2Speed = +document.getElementById('d2-speed').value;
-  let d2Limit = +document.getElementById('d2-limit').value;
-  let d3Speed = +document.getElementById('d3-speed').value;
-  let d3Limit = +document.getElementById('d3-limit').value;
+  let speed1 = +document.getElementById('speed1').value;
+  let limit1 = +document.getElementById('limit1').value;
+  let speed2 = +document.getElementById('speed2').value;
+  let limit2 = +document.getElementById('limit2').value;
+  let speed3 = +document.getElementById('speed3').value;
+  let limit3 = +document.getElementById('limit3').value;
+  //let d2Speed = +document.getElementById('d2-speed').value;
+  //let d2Limit = +document.getElementById('d2-limit').value;
+  //let d3Speed = +document.getElementById('d3-speed').value;
+  //let d3Limit = +document.getElementById('d3-limit').value;
 
   // Calculate & Output Ticket Types
-  document.getElementById('d1-speed').innerHTML = speedToFine(d1Speed);
-  document.getElementById('d1-limit').innerHTML = speedToFine(d1Limit);
-  document.getElementById('d2-speed').innerHTML = speedToFine(d2Speed);
-  document.getElementById('d2-limit').innerHTML = speedToFine(d2Limit);
-  document.getElementById('d3-speed').innerHTML = speedToFine(d3Speed);
-  document.getElementById('d3-limit').innerHTML = speedToFine(d3Limit);
+  document.getElementById('ticket1').innerHTML = speedToFine(speed1, limit1);
+  document.getElementById('ticket2').innerHTML = speedToFine(speed2, limit2);
+  document.getElementById('ticket3').innerHTML = speedToFine(speed3, limit3);
+
+
+  //document.getElementById('d2-speed').innerHTML = speedToFine(d2Speed);
+  //document.getElementById('d2-limit').innerHTML = speedToFine(d2Limit);
+  //document.getElementById('d3-speed').innerHTML = speedToFine(d3Speed);
+  //document.getElementById('d3-limit').innerHTML = speedToFine(d3Limit);
 }
-function speedToFine(speedLimit) {
-  if (speedLimit > speedLimit + 40) {
+function speedToFine(speed,limit) {
+  if (speed > limit + 40) {
     return 'Really Big Ticket';
-  } else if (speedLimit > speedLimit + 20) {
+  } else if (speed > limit + 20) {
     return 'Big Ticket';
-  } else if (speedLimit > speedLimit) {
+  } else if (speed > limit) {
     return 'Small Ticket';
   } else {
     return 'No Ticket';
